0d849528efd8c8d625948fb8f7501f1dbe6e896b
Aswini <patilashwinipatil7259@gmail.com>
2024-10-18 14:39:51 +0530

    first commit

---
+++ first1.js
@@ -0,0 +1,56 @@
+//btn1
+document.body.style.backgroundColor='white';
+btn1.addEventListener('mouseenter',function(){
+    btn1.style.color='blue';
+    btn1.style.backgroundcolor='black';
+document.body.style.backgroundColor='blue';
+});
+document.body.style.backgroundColor='green';
+btn1.addEventListener('mouseleave',function(){
+    btn1.style.color='black';
+    btn1.style.backgroundcolor='black';
+    document.body.style.backgroundColor='white';
+});
+//btn2
+
+btn2.addEventListener('mouseenter',function(){
+    btn2.style.color='pink';
+    document.body.style.backgroundColor='pink';
+});
+
+btn2.addEventListener('mouseleave',function(){
+    btn2.style.color='black';
+    document.body.style.backgroundColor='white';
+});
+//btn3
+
+btn3.addEventListener('mouseenter',function(){
+    btn3.style.color='orange';
+    document.body.style.backgroundColor='orange';
+});
+
+btn3.addEventListener('mouseleave',function(){
+    btn3.style.color='black';
+    document.body.style.backgroundColor='white';
+});
+//btn4
+btn4.addEventListener('mouseenter',function(){
+    btn4.style.color='yellow';
+    document.body.style.backgroundColor='yellow';
+});
+
+btn4.addEventListener('mouseleave',function(){
+    btn4.style.color='black';
+    document.body.style.backgroundColor='white';
+});
+//btn5
+btn5.addEventListener('mouseenter',function(){
+    btn5.style.color='grey';
+    document.body.style.backgroundColor='grey';
+});
+
+btn5.addEventListener('mouseleave',function(){
+    btn5.style.color='black';
+    document.body.style.backgroundColor='white';
+});
+
